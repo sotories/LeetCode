eb1e4388291f84583195bf37427bbc4096a7bea5
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-05-25 19:30:58 +0900

    Time: 57 ms (45.82%), Space: 55.1 MB (46.81%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+    if (functions.length === 0) {
+        return function (x) { return x }
+    }
+
+    let result;
+    return function (x) {
+        result = x;
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result)
+        }
+        return result;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
