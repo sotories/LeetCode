9ac455be3ddf9ad80f9c1071ef770c5ef60b8d00
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-06-25 16:25:29 +0900

    Time: 0 ms (100%), Space: 56.7 MB (20.79%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -3,17 +3,17 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
-    const map = new Map();
+    const set = new Set();
 
     while (n !== 1) {
         const newNum = `${n}`.split("").map(v => (Number(v)) * Number(v)).reduce((acc, cur) => acc += cur, 0);
         if (newNum === 1) {
             return true;
         }
-        if (map.has(newNum)) {
+        if (set.has(newNum)) {
             return false;
         } else {
-            map.set(newNum, true)
+            set.add(newNum)
             n = newNum
         }
     }
