0230442172e81b64a59d07ecee39a80a8b3dfafd
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-06-25 17:43:58 +0900

    Time: 6 ms (68.19%), Space: 57.2 MB (11.67%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    const sMap = new Map();
+    const tMap = new Map();
+
+    if (s.length === 1) return true;
+
+    for (let i = 0; i < s.length; i++) {
+        if (!sMap.get(s[i])) {
+            sMap.set(s[i], t[i])
+        }
+
+        if (t[i] !== sMap.get(s[i])) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (!tMap.get(t[i])) {
+            tMap.set(t[i], s[i])
+        }
+
+        if (s[i] !== tMap.get(t[i])) {
+            return false;
+        }
+    }
+    return true;
+};
