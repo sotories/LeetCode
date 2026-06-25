89b18b6414d6224c889bd1b97e5379f443d97e35
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-06-25 16:00:16 +0900

    Time: 7 ms (44.29%), Space: 64.4 MB (14.84%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+    const symbols = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"]
+    ];
+
+    let result = ""
+
+    for (const [int, roman] of symbols) {
+        while (num >= int) {
+            num -= int
+            result += roman
+        }
+    }
+    return result
+};
