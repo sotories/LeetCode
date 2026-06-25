70195b69a2feca4ff3c30c31700de1077ee32194
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-06-25 17:57:28 +0900

    Time: 19 ms (32.48%), Space: 61 MB (10.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,8 +5,18 @@
  */
 var isAnagram = function (s, t) {
     if (s.length !== t.length) return false;
+    const map = new Map();
 
-    s = [...s].sort().join("");
-    t = [...t].sort().join("");
-    return s === t
+    for (const char of s) {
+        map.set(char, (map.get(char) || 0) + 1)
+    }
+
+    for (const char of t) {
+        if (!map.get(char) || map.get(char) < 1) {
+            return false
+        }
+
+        map.set(char, map.get(char) - 1)
+    }
+    return true
 };
