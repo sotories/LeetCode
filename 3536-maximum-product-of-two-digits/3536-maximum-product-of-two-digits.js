eb78203ffb9b34563f4f8f1b58d40efd65357d22
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-08-05 23:24:01 +0900

    Time: 1 ms (74.39%), Space: 58 MB (5.33%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var maxProduct = function (n) {
+    let max1 = 0, max2 = 0;
+
+    for (; n; n = Math.floor(n / 10)) {
+        let d = n % 10;
+        if (d >= max1) {
+            max2 = max1;
+            max1 = d;
+        } else if (d > max2)
+            max2 = d;
+    }
+
+    return max1 * max2;
+};
