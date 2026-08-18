aa2129f0576d71a3cc77553d476985e4ec008abd
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-08-18 23:38:35 +0900

    Time: 2 ms (77.36%), Space: 55.9 MB (54.18%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maximumLengthSubstring = function(s) {
+    let res = 0;
+    let fq = Array(26).fill(0);
+
+    for (let l = 0, r = 0; r < s.length; r++) {
+        fq[s.charCodeAt(r) - 97]++;
+
+        while (fq[s.charCodeAt(r) - 97] > 2)
+            fq[s.charCodeAt(l++) - 97]--;
+
+        res = Math.max(res, r - l + 1);
+    }
+
+    return res;
+};
